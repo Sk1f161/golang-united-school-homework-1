38d0254c6b47286679bcf64a36657f670b3acdf2
Vitaliy Popov <sk1f.tag@gmail.com>
2022-02-17 18:44:09 +0300

    Test 1

--- solution.go
+++ solution.go
@@ -1,12 +1,10 @@
 package solution
 
 import (
-	"fmt"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
-	hello := emoji.Sprint("Hello world :world_map:")
-	fmt.Println(hello)
+	hello := emoji.Sprint("Hello :world_map:!")
+	return hello
 }
